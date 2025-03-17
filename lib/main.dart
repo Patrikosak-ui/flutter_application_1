68d09d284e55070fd94306ba614713e4f8f64aef
Patrikosak-ui <patrikcarska3@gmail.com>
2025-03-17 14:07:55 +0100

    datum

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart'; // added for locale initialization
 import 'login_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await initializeDateFormatting('cs_CZ', null); // initialize Czech locale formatting
   runApp(const MyApp());
 }
 
